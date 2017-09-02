497ca55f407e3033eb630ec3a8e325a6a4238bbe
zanocom <zanocom@gmail.com>
2017-09-02 22:47:48 +0200

    Deploying changes

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,8 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = False
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_RETENTION = [".hg", ".git", ".bzr"]
 
 # Following items are often useful when publishing
 
